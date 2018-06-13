0dd67e093462c83a6e516c28443ec03bc017ad5d
tangzzz-fan <csqingyang@foxmail.com>
2018-06-13 14:57:25 +0800

    MOD:Update

---
+++ TGDownloader/TGDownloader/Classes/Controller/TestNavViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestNavViewController.h
+//  TGDownloader
+//
+//  Created by MacPro on 2018/6/8.
+//  Copyright © 2018年 Centaline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestNavViewController : UINavigationController
+
+@end
---
+++ TGDownloader/TGDownloader/Classes/Controller/TestNavViewController.m
@@ -0,0 +1,21 @@
+//
+//  TestNavViewController.m
+//  TGDownloader
+//
+//  Created by MacPro on 2018/6/8.
+//  Copyright © 2018年 Centaline. All rights reserved.
+//
+
+#import "TestNavViewController.h"
+
+@interface TestNavViewController ()
+
+@end
+
+@implementation TestNavViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
